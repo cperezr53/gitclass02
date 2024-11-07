4621f4adbc429102ddf7b561b692ea96a53959a9
Carlos Perez <cperezr53@gmail.com>
2024-11-06 19:57:30 -0500

    Third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        for (int i=0,i<100,i++){
-            System.out.println("Hello world");
+
         }
-    }
 }
